b3a50b17671a20d517dbaaf586f8cda082d50807
zmonte <superuser.x64@gmail.com>
2016-02-24 23:00:09 -0500

    Rename vfd.h to vfd/vfd.h

---
+++ vfd/vfd.h
@@ -0,0 +1,18 @@
+// vfd.h
+
+#define USEC_DELAY 40
+
+/*
+ * Commandset:
+ */
+const int CURSOR_BLINK = 0x07u;
+const int BACKSPACE = 0x08u;
+const int CURSOR_RIGHT = 0x09u;
+const int CURSOR_DOWN = 0x0Au;
+// const int UNK = 0x0Bu;
+// const int UNK = 0x0Cu;
+const int CURSOR_HOME = 0x0Du;
+const int CURSOR_OFF = 0x0Eu;
+// const int UNK = 0x0Fu;
+// const int UNK = 0x10u;
+
